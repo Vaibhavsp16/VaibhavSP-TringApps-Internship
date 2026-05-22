aa449a738781656f7f37c079b0d65eae80aeddd9
Vaibhav S P <vaibhavsp16@gmail.com>
2026-05-22 17:31:10 +0530

    feat: provisioned S3 data analytics bucket and automated file upload

--- IAM/main.tf
+++ IAM/main.tf
@@ -23,3 +23,36 @@ resource "aws_iam_user" "vaibhav_new" {
     Owner       = "Vaibhav"
   }
 }
+
+# Generate Programmatic Access Keys for the new user
+resource "aws_iam_access_key" "vaibhav_new_keys" {
+  user = aws_iam_user.vaibhav_new.name
+}
+
+output "access_key_id" {
+  value = aws_iam_access_key.vaibhav_new_keys.id
+}
+
+output "secret_access_key" {
+  value     = aws_iam_access_key.vaibhav_new_keys.secret
+  sensitive = true # This hides the secret key in the terminal until you explicitly ask for it
+}
+
+# --- LAB 2: S3 BUCKET & FILE UPLOAD ---
+
+# 1. Create the S3 Bucket
+resource "aws_s3_bucket" "my_data_bucket" {
+  bucket = "vaibhav-internship-data-bucket-1612" # <-- Change the numbers at the end!
+
+  tags = {
+    Environment = "Development"
+    Purpose     = "Data Analytics Storage"
+  }
+}
+
+# 2. Upload the Text File to the Bucket
+resource "aws_s3_object" "file_upload" {
+  bucket = aws_s3_bucket.my_data_bucket.id # Tells Terraform which bucket to target
+  key    = "my_test_file.txt"              # The name the file will have inside AWS
+  source = "my_test_file.txt"              # The local file we just created in Step 1
+}
